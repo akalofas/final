bb1cc5081482badb6b291063b40b0485a8833d97
Angelos Kalofas <kaloeake@icloud.com>
2023-03-15 19:07:01 -0400

    final project

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,11 @@
-
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    return (
+        <div class="alert alert-dark" role="alert" className='alert alert-secondary'>
+            <span>Budget: £ {budget}</span>
+        </div>
+    );
+};
+export default Budget;
